643936a264753a87f0578beca43523b16cbe9bd3
pnchw <pancheewa.thpm@hotmail.com>
2025-03-11 00:59:37 +0700

    nineth-commit

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -52,6 +52,7 @@ function Portfolio() {
   return (
     <motion.section id="portfolio" ref={ref} className="select-none py-32 text-white" style={{ backgroundImage }}>
       <div className="container mx-auto grid lg:grid-cols-8">
+
         <div className="mr-12 mb-12 md:mb-0 lg:col-span-3">
           <motion.h2 
             className="text-6xl mb-10 font-bold sticky top-20"
@@ -62,61 +63,62 @@ function Portfolio() {
             My <span className="text-blue-400">Projects</span>
           </motion.h2>
         </div>
-          {/* project list */}
-          <div className="lg:col-span-5">
+
+        {/* project list */}
+        <div className="lg:col-span-5">
           {projects.map((project, index) => (
-            <motion.div
-              className="cursor-pointer mb-8 group"
-              key={project.id}
-              onClick={() => setSelectedProject(project)}
-              initial={{ opacity: 0, y: 50 }}
-              animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y:50 }}
-              transition={{ delay: 0.5 + index * 0.2, duration: 0.6 }}
+          <motion.div
+            className="cursor-pointer mb-8 group"
+            key={project.id}
+            onClick={() => setSelectedProject(project)}
+            initial={{ opacity: 0, y: 50 }}
+            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y:50 }}
+            transition={{ delay: 0.5 + index * 0.2, duration: 0.6 }}
+          >
+            <p className="text-gray-400 text-lg mb-2">{project.year}</p>
+            <h3
+              className={`text-3xl font-semibold group-hover:text-blue-400 transition-colors 
+                        ${selectedProject.id === project.id ? "text-blue-200" : "text-gray-500"} duration-300`}
             >
-              <p className="text-gray-400 text-lg mb-2">{project.year}</p>
-              <h3
-                className={`text-3xl font-semibold group-hover:text-blue-400 transition-colors 
-                          ${selectedProject.id === project.id ? "text-blue-200" : "text-gray-500"} duration-300`}
+              {project.title}
+            </h3>
+
+            {/* underline effect */}
+            {selectedProject.id === project.id && (
+              <div className="border-b-2 border-blue-200 my-4"></div>
+            )}
+
+            {/* description */}
+            {selectedProject.id === project.id && (
+              <motion.p 
+                className="text-gray-400"
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
               >
-                {project.title}
-              </h3>
-
-              {/* underline effect */}
-              {selectedProject.id === project.id && (
-                <div className="border-b-2 border-blue-200 my-4"></div>
-              )}
-
-              {/* description */}
-              {selectedProject.id === project.id && (
-                <motion.p 
-                  className="text-gray-400"
-                  initial={{ opacity: 0, y: -10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.8, ease: "easeInOut" }}
-                >
-                  {project.description}
-                </motion.p>
-              )}
-
-              {/* selected image project with fading in-out effect */}
-              {selectedProject.id === project.id && (
-                <motion.div
-                  className="mt-6 flex items-center justify-center"
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.5, ease: "easeInOut"}}
-                >
-                  <img 
-                    className="my-auto rounded-xl shadow-lg"
-                    src={project.src}
-                    width={800}
-                    height={450}
-                    alt={selectedProject.title}
-                  ></img>
-                </motion.div>
-              )}
-            </motion.div>
+                {project.description}
+              </motion.p>
+            )}
+          </motion.div>
           ))}
+        
+
+            {/* selected image project with fading in-out effect */}
+            <motion.div
+              className="mt-20 flex items-center justify-center"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut"}}
+            >
+              <Image
+                className="my-auto rounded-xl shadow-lg"
+                src={selectedProject.src}
+                width={800}
+                height={450}
+                alt={selectedProject.title}
+                unoptimized={true}
+              />
+            </motion.div>
           </div>
       </div>
     </motion.section>
